1097108333b2f9d05be444e03b340c3986702426
Balla Aarathisree <121624731+Aarathi1535@users.noreply.github.com>
2025-12-18 12:21:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ AZURE_KEY      = os.getenv("AZURE_KEY")
 
 GROQ_API_KEY   = os.getenv("GROQ_API_KEY")
 
-GROQ_MODEL_EXTRACTION = "llama-3.1-8b-instant"
+GROQ_MODEL_EXTRACTION = "llama-3.3-70b-versatile"
 GROQ_MODEL_GRADING    = "llama-3.1-8b-instant"
+
